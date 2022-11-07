79c04384978d96106ca5127c26f352364d9752ad
Rylan <rylanschaeffer@gmail.com>
2022-11-06 19:25:16 -0800

    Finished HW5 Part 1

--- hw5/README.txt
+++ hw5/README.txt
@@ -1,2 +1,2 @@
 # Put your SUID number here (e.g., id: 01234567)
-id: <your_last_8-digit_student_id_number>
+id: 05756291
--- hw5/part1/src/prob1/cart.rs
+++ hw5/part1/src/prob1/cart.rs
@@ -22,9 +22,104 @@ use crate::prob1::server::internal_login;
 //
 // Here T denotes a type. Note that each T can be a different type.
 //===== BEGIN_CODE =====//
-pub struct Cart {}
-pub struct Empty {}
-pub struct NonEmpty {}
-pub struct Checkout {}
+pub struct Cart {  }
+pub struct Empty {
+    acct_num_val : u32 ,
+    tot_cost_val : u32
+}
+pub struct NonEmpty {
+    acct_num_val : u32 ,
+    tot_cost_val : u32
+}
+pub struct Checkout {
+    acct_num_val : u32 ,
+    tot_cost_val : u32
+}
+
+impl Cart {
+    pub fn login(id: String, pw: String) -> Result<Empty,()> {
+        let user: Option<u32> = internal_login(id, pw);
+        match user {
+            None => return Err(()),
+            Some(val) => return Ok(Empty{acct_num_val: val, tot_cost_val: 0}),
+        }
+    }
+}
+
+impl Empty {
+    pub fn add_item(self, cost: u32) -> NonEmpty {
+        return NonEmpty{
+            acct_num_val: self.acct_num_val,
+            tot_cost_val: self.tot_cost_val + cost
+        };
+    }
+
+    pub fn acct_num(&self) -> u32 {
+        return self.acct_num_val
+    }
+
+    pub fn tot_cost(&self) -> u32 {
+        return self.tot_cost_val
+    }
+}
+
+impl NonEmpty {
+
+    pub fn add_item(self, cost: u32) -> NonEmpty {
+        return NonEmpty{
+            acct_num_val: self.acct_num_val,
+            tot_cost_val: self.tot_cost_val + cost
+        };
+    }
+
+    pub fn checkout(self) -> Checkout {
+        return Checkout{
+            acct_num_val: self.acct_num_val,
+            tot_cost_val: self.tot_cost_val
+        };
+    }
+
+    pub fn clear_items(self) -> Empty {
+        return Empty {
+            acct_num_val: self.acct_num_val ,
+            tot_cost_val: 0
+        };
+    }
+
+    pub fn acct_num(&self) -> u32 {
+        return self.acct_num_val;
+    }
+
+    pub fn tot_cost(&self) -> u32 {
+        return self.tot_cost_val;
+    }
+}
+
+impl Checkout {
+
+    pub fn order(self) -> Empty {
+        return Empty {
+            acct_num_val : self.acct_num_val,
+            tot_cost_val : 0,
+        }
+    }
+
+    pub fn cancel(self) -> NonEmpty {
+        return NonEmpty {
+            acct_num_val : self.acct_num_val,
+            tot_cost_val : self.tot_cost_val,
+        }
+    }
+
+    pub fn acct_num(&self) -> u32 {
+        return self.acct_num_val
+    }
+
+    pub fn tot_cost(&self) -> u32 {
+        return self.tot_cost_val
+    }
+
+}
+
 
 //===== END_CODE =====//
