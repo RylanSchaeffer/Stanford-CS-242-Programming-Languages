4c87dd5aac9595e902baaa0821a3caa4a9c53f09
Rylan <rylanschaeffer@gmail.com>
2022-12-07 15:05:46 -0800

    Finished HW8 Prob 4&5

--- hw8/prob04.lean
+++ hw8/prob04.lean
@@ -6,5 +6,16 @@ lemma transitive_left (op : Op) (e1 e1' e2 : Expr) :
   → (Expr.Op op e1 e2 ↦* Expr.Op op e1' e2) :=
 begin
   -- FILL IN HERE.
-  sorry
+  intros t,
+  induction t with crefl cstep a b c d e,
+  case evals.CRefl {
+    apply evals.CRefl,
+  },
+  case evals.CStep {
+    apply evals.CStep,
+      show Expr, from Expr.Op op a e2,  -- This solves the 3rd branch
+      apply eval.ELeft,
+      exact c,
+      exact e,
+  }
 end
--- hw8/prob05.lean
+++ hw8/prob05.lean
@@ -5,6 +5,18 @@ lemma transitive_right (op : Op) (e1 e2 e2' : Expr) :
   (val e1) → (e2 ↦* e2')
   → (Expr.Op op e1 e2 ↦* Expr.Op op e1 e2') :=
 begin
-  -- FILL IN HERE.
-  sorry
+  intros e1val t,
+  induction t with crefl cstep a b c d e,
+  case evals.CRefl {
+    apply evals.CRefl,
+  },
+  case evals.CStep {
+    apply evals.CStep,
+      show Expr,
+      from Expr.Op op e1 a,  -- This solves the 3rd branch
+      apply eval.ERight,
+      exact c,
+      exact e1val,
+      exact e,
+  }
 end
