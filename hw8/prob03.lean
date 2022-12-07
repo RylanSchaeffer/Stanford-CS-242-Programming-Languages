222679856492fbfce4553a644a9bce31ae7f24c0
Rylan <rylanschaeffer@gmail.com>
2022-12-06 20:23:05 -0800

    Finished Prob 3

--- hw8/prob03.lean
+++ hw8/prob03.lean
@@ -13,6 +13,44 @@ import .prob01
 theorem progress :
   ∀ e : Expr, (val e) ∨ (∃ e', e ↦ e') :=
 begin 
-  -- FILL IN HERE.
-  sorry
+  intros,
+  induction e with n op e1 e2 ihe1 ihe2,
+  left,  -- Base case i.e. natural num
+  apply val.VNum,
+  right,  -- Recursive case i.e. step
+  cases ihe1 with e1val e1expr,  -- Split inductive hypotheses for e1 into either a N or an expr
+  {
+    cases ihe2 with e2val e2expr,
+    {  -- This assumes e1 and e2 are both vals.
+      cases e1val with n1,
+      cases e2val with n2,
+      existsi Expr.Num (apply_op op n1 n2),
+      apply eval.EOp,
+    },
+    {  -- This assumes e1 is a val, but e2 is an expr.
+      cases e2expr with e2expr1 e2expr2,
+      simp*,
+      existsi Expr.Op op e1 e2expr1,
+      apply eval.ERight,
+      assumption,
+      assumption,
+    }
+  },
+  {
+    cases ihe2 with e2val e2expr,
+    {   -- This assumes e1 is an expr, but e2 is a val.
+      cases e1expr with e1expr1 e1expr2,
+      simp*,
+      existsi Expr.Op op e1expr1 e2,
+      apply eval.ELeft,
+      assumption,
+    },
+    {   -- This assumes both e1 and e2 are expr.
+      cases e1expr with e1expr1 e1expr2,
+      simp*,
+      existsi Expr.Op op e1expr1 e2,
+      apply eval.ELeft,
+      assumption,
+    }
+  }
 end 
