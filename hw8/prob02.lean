b9742e44f020e86f2295917f231f6d9aade90e85
Rylan <rylanschaeffer@gmail.com>
2022-12-06 16:22:37 -0800

    Finished HW8 Problem 2

--- hw8/prob02.lean
+++ hw8/prob02.lean
@@ -2,16 +2,26 @@
 import .src.lnat
 
 def e1 : Expr :=
-  -- FILL IN HERE.
-  sorry
+    (Expr.Op Op.Add
+      (Expr.Num 1)
+      (Expr.Op Op.Add ((Expr.Op Op.Add  (Expr.Num 2)
+                                        (Expr.Num 3)))
+                      (Expr.Num 4))
+    )
 
 def e2 : Expr :=
-  -- FILL IN HERE.
-  sorry
+    (Expr.Op Op.Add
+      (Expr.Num 1)
+      (Expr.Op Op.Add (Expr.Num 5) (Expr.Num 4))
+    )
 
 lemma eval_example :
   e1 ↦ e2 :=
 begin
-  -- FILL IN HERE.
-  sorry
+  apply eval.ERight,
+  apply eval.ELeft,
+  apply eval.EOp,
+  apply val.VNum,
+  -- apply eval.ELeft,
+  -- apply eval.EOp,
 end
